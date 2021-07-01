b608edc36cb57f68d840ece6d14e2d357fa68c41
Filipp Honcharenko <akstoplay@gmail.com>
2021-07-01 14:17:35 +0300

    bot init

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const TNB = require("node-telegram-bot-api");
+const TOKEN = "1873305865:AAHuGAc_kGzXt0KIdKSPddOdWS2qyxdfk4A";
+
+const bot = new TNB(TOKEN, {
+    polling: true
+});
+
+bot.on("message", (msg) => {
+    bot.sendMessage(msg.chat.id, 'Hello,' + msg.from.first_name + '!');
+});
+
+
 console.log("Test case: #4");
