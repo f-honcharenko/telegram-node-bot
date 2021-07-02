69609560dec676137f0abbb8f5dc2fc597850650
Filipp Honcharenko <akstoplay@gmail.com>
2021-07-02 14:53:20 +0300

    express3

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-// console.log(process.env.GOOGLE_CLOUD_REGION);
-// console.log(process.env.GOOGLE_CLOUD_PROJECT_ID);
-// console.log(process.env.FUNCTION_TARGET);
-
-
-// const {
-//     Telegraf
-// } = require('telegraf')
-
-// const TOKEN = process.env.TOKEN || "1873305865:AAHuGAc_kGzXt0KIdKSPddOdWS2qyxdfk4A"
-// const GOOGLE_CLOUD_REGION = process.env.GOOGLE_CLOUD_REGION || "europe-west1";
-// const GOOGLE_CLOUD_PROJECT_ID = process.env.GOOGLE_CLOUD_PROJECT_ID || "node-telegram-bot-318510";
-// const PORT = process.env.PORT || "8080";
-
-// const bot = new Telegraf(TOKEN);
-// bot.start((ctx) => ctx.reply(`Welcome to the most silly bot you'll ever see`));
-// bot.help((ctx) => ctx.reply(`Write anything to me and I'll repeat it :)`));
-// bot.on('text', (ctx) => ctx.reply(ctx.message.text)); //listen to every text message
-// bot.on('message', ctx => ctx.reply('Command not recognized')); //avoid timeouts with unsupported commands
-// bot.telegram.setWebhook(
-//     `https://${GOOGLE_CLOUD_REGION}-${GOOGLE_CLOUD_PROJECT_ID}.cloudfunctions.net/${process.env.FUNCTION_TARGET}` //FUNCTION_TARGET is reserved Google Cloud Env
-// );
-
-// exports.telegramBotWebhook = (req, res) => {
-//     bot.handleUpdate(req.body, res);
-// };
-
-// console.log("Test case: #7");
-
-
+const {
+    Telegraf
+} = require('telegraf')
+const config = require('config');
 const express = require('express')
+
+
 const app = express()
-const port = process.env.PORT || "8080";
+const bot = new Telegraf(config.get("token"));
+const port = process.env.PORT || config.get("port");
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -39,7 +16,17 @@ app.post('/webhook', (req, res) => {
     res.send('post')
     console.log("POST [" + req.ip + "]");
 })
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+bot.start((ctx) => ctx.reply(`Welcome to the most silly bot you'll ever see`));
+bot.help((ctx) => ctx.reply(`Write anything to me and I'll repeat it :)`));
+bot.on('text', (ctx) => ctx.reply(ctx.message.text)); //listen to every text message
+
+bot.on('message', ctx => ctx.reply('Command not recognized')); //avoid timeouts with unsupported commands
+bot.telegram.setWebhook(config.get("webhook-link"));
+
+exports.telegramBotWebhook = (req, res) => {
+    bot.handleUpdate(req.body, res);
+};
